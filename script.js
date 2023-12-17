b7ea2e1d2bda71fc277305919b1acd5137a2e951
Andrey Soldatkin <andrey.soldatkin@mobirate.com>
2023-12-17 23:34:36 +0400

    5

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     loadItems();
     loadBalance();
     loadHistory();
+    loadTipPercentage();
 
     var modal = document.getElementById("settingsModal");
     var historyModal = document.getElementById("historyModal");
@@ -29,54 +30,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     window.onclick = function(event) {
-        if (event.target == modal) {
+        if (event.target == modal || event.target == historyModal) {
             modal.style.display = "none";
-        }
-        if (event.target == historyModal) {
             historyModal.style.display = "none";
         }
     }
 
-    document.getElementById("addNewItemButton").onclick = function() {
-        var name = document.getElementById('newItemName').value;
-        var price = document.getElementById('newItemPrice').value;
-        var people = document.getElementById('newItemPeople').value || 1;
-
-        if (name && price) {
-            var items = JSON.parse(localStorage.getItem('items') || '[]');
-            items.push({ name: name, price: parseFloat(price), people: parseInt(people) });
-            localStorage.setItem('items', JSON.stringify(items));
-            loadItems();
-            loadSettingsItems();
-        }
-    }
-
-    document.getElementById("updateBalanceButton").onclick = function() {
-        var newBalance = parseFloat(document.getElementById('balanceInput').value);
-        if (!isNaN(newBalance)) {
-            localStorage.setItem('balance', newBalance);
-            loadBalance();
-        }
-    }
-
-    document.getElementById("calculateTotalButton").onclick = function() {
-        calculateTotal();
-    }
-
-    document.getElementById("subtractFromBalanceButton").onclick = function() {
-        var total = calculateTotal();
-        var balance = parseFloat(localStorage.getItem('balance') || 0);
-        balance -= total;
-        localStorage.setItem('balance', balance);
-        saveTransaction(total);
-        loadBalance();
-        loadHistory();
-    }
-
-    document.getElementById("clearHistoryButton").onclick = function() {
-        localStorage.setItem('history', JSON.stringify([]));
-        loadHistory();
-    }
+    document.getElementById("addNewItemButton").onclick = addNewItem;
+    document.getElementById("updateBalanceButton").onclick = updateBalance;
+    document.getElementById("updateTipButton").onclick = updateTipPercentage;
+    document.getElementById("calculateTotalButton").onclick = calculateTotalWithTips;
+    document.getElementById("subtractFromBalanceButton").onclick = subtractFromBalance;
+    document.getElementById("clearHistoryButton").onclick = clearHistory;
 });
 
 function loadItems() {
@@ -91,7 +56,7 @@ function loadItems() {
         checkbox.dataset.people = item.people;
 
         var label = document.createElement('label');
-        label.textContent = item.name + ' (' + item.price + ' руб.)';
+        label.textContent = item.name + ' (' + item.price + ')';
 
         var peopleInput = document.createElement('input');
         peopleInput.type = 'number';
@@ -109,6 +74,20 @@ function loadItems() {
     });
 }
 
+function addNewItem() {
+    var name = document.getElementById('newItemName').value;
+    var price = document.getElementById('newItemPrice').value;
+    var people = document.getElementById('newItemPeople').value || 1;
+
+    if (name && price) {
+        var items = JSON.parse(localStorage.getItem('items') || '[]');
+        items.push({ name: name, price: parseFloat(price), people: parseInt(people) });
+        localStorage.setItem('items', JSON.stringify(items));
+        loadItems();
+        loadSettingsItems();
+    }
+}
+
 function loadSettingsItems() {
     var items = JSON.parse(localStorage.getItem('items') || '[]');
     var settingsList = document.getElementById('settingsList');
@@ -118,24 +97,47 @@ function loadSettingsItems() {
         div.className = 'settings-item';
 
         var span = document.createElement('span');
-        span.textContent = item.name + ' (' + item.price + ' руб.) на ' + item.people + ' чел.';
+        span.textContent = ' ' + item.name + ' (' + item.price + ') / ' + item.people + '';
 
         var deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Удалить';
+        deleteButton.textContent = 'Delete';
         deleteButton.onclick = function() {
             deleteItem(index);
         };
 
+		div.appendChild(deleteButton);
         div.appendChild(span);
-        div.appendChild(deleteButton);
+        
         settingsList.appendChild(div);
     });
+}
+
+function updateBalance() {
+    var newBalance = parseFloat(document.getElementById('balanceInput').value);
+    if (!isNaN(newBalance)) {
+        localStorage.setItem('balance', newBalance);
+        loadBalance();
+    }
 }
 
 function loadBalance() {
     var balance = parseFloat(localStorage.getItem('balance') || 0);
     document.getElementById('balanceDisplay').textContent = balance.toFixed(2);
     document.getElementById('balanceDisplayModal').textContent = balance.toFixed(2);
+}
+
+function updateTipPercentage() {
+    var tipPercentage = parseFloat(document.getElementById('tipPercentage').value);
+    if (!isNaN(tipPercentage)) {
+        localStorage.setItem('tipPercentage', tipPercentage);
+        loadTipPercentage();
+    }
+}
+
+function loadTipPercentage() {
+    var tipPercentage = parseFloat(localStorage.getItem('tipPercentage') || 0);
+    document.getElementById('tipDisplay').textContent = tipPercentage + '%';
+    document.getElementById('applyTip').checked = false;
 }
 
 function calculateTotal() {
@@ -150,8 +152,64 @@ function calculateTotal() {
             total += price / people;
         }
     });
-    document.getElementById('total').textContent = 'Итоговая сумма: ' + total.toFixed(2) + ' руб.';
     return total;
+}
+
+function calculateTotalWithTips() {
+    var total = calculateTotal();
+    if (document.getElementById('applyTip').checked) {
+        var tipPercentage = parseFloat(localStorage.getItem('tipPercentage') || 0);
+        var tipAmount = total * (tipPercentage / 100);
+        total += tipAmount;
+    }
+    document.getElementById('total').textContent = 'Sum: ' + total.toFixed(2) + '';
+    return total;
+}
+
+function subtractFromBalance() {
+    var total = calculateTotalWithTips();
+    var balance = parseFloat(localStorage.getItem('balance') || 0);
+    balance -= total;
+    localStorage.setItem('balance', balance);
+    saveTransaction(total);
+    loadBalance();
+    loadHistory();
+}
+
+function saveTransaction(total) {
+    var history = JSON.parse(localStorage.getItem('history') || '[]');
+    var date = new Date().toLocaleString();
+    var involvedItems = Array.from(document.querySelectorAll('#itemsList div'))
+                            .filter(div => div.querySelector('input[type=checkbox]').checked)
+                            .map(div => {
+                                var label = div.querySelector('label').textContent;
+                                var peopleCount = div.querySelector('.people-count').value;
+                                return `${label} / ${peopleCount}`;
+                            });
+
+    if (document.getElementById('applyTip').checked) {
+        var tipPercentage = parseFloat(localStorage.getItem('tipPercentage') || 0);
+        involvedItems.push(`Tips: ${tipPercentage}%`);
+    }
+
+    history.push({ date: date, total: total.toFixed(2), items: involvedItems });
+    localStorage.setItem('history', JSON.stringify(history));
+}
+
+function loadHistory() {
+    var history = JSON.parse(localStorage.getItem('history') || '[]');
+    var historyList = document.getElementById('historyList');
+    historyList.innerHTML = '';
+    history.forEach(function(entry) {
+        var div = document.createElement('div');
+        div.textContent = `${entry.date}: ${entry.total} - [${entry.items.join(', ')}]`;
+        historyList.appendChild(div);
+    });
+}
+
+function clearHistory() {
+    localStorage.setItem('history', JSON.stringify([]));
+    loadHistory();
 }
 
 function deleteItem(index) {
@@ -169,30 +227,4 @@ function updateItemPeople(index, newPeople) {
         localStorage.setItem('items', JSON.stringify(items));
         loadItems();
     }
-}
-
-function loadHistory() {
-    var history = JSON.parse(localStorage.getItem('history') || '[]');
-    var historyList = document.getElementById('historyList');
-    historyList.innerHTML = '';
-    history.forEach(function(entry) {
-        var div = document.createElement('div');
-        div.textContent = `${entry.date}: ${entry.total} руб. - [${entry.items.join(', ')}]`;
-        historyList.appendChild(div);
-    });
-}
-
-function saveTransaction(total) {
-    var history = JSON.parse(localStorage.getItem('history') || '[]');
-    var date = new Date().toLocaleString();
-    var involvedItems = Array.from(document.querySelectorAll('#itemsList div'))
-                            .filter(div => div.querySelector('input[type=checkbox]').checked)
-                            .map(div => {
-                                var label = div.querySelector('label').textContent;
-                                var peopleCount = div.querySelector('.people-count').value;
-                                return `${label} на ${peopleCount} чел.`;
-                            });
-
-    history.push({ date: date, total: total.toFixed(2), items: involvedItems });
-    localStorage.setItem('history', JSON.stringify(history));
 }
